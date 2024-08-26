4cab109be9625611a7d97ad75d0e155b09f3e284
unknown <ahmadfuadoorakam@gmail.com>
2024-08-26 08:59:52 -0700

    fix all error

--- client/src/components/AddImageIcon.jsx
+++ client/src/components/AddImageIcon.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState,useRef } from 'react';
 import axios from 'axios';
 
 const AddImageIcon = ({ onImageUpload }) => {
     const [selectedFile, setSelectedFile] = useState(null);
+    const [imagePreview, setImagePreview] = useState(null);
+    const fileInputRef = useRef(null); // Ref for file input
 
     const handleFileChange = (event) => {
-        setSelectedFile(event.target.files[0]);
+        const file = event.target.files[0];
+        setSelectedFile(file);
+        if (file) {
+            setImagePreview(URL.createObjectURL(file));
+        } else {
+            setImagePreview(null);
+        }
     };
 
     const handleUpload = async () => {
@@ -17,16 +25,32 @@ const AddImageIcon = ({ onImageUpload }) => {
         try {
             const response = await axios.post('http://localhost:5000/api/image/upload', formData);
             onImageUpload(response.data.imageUrl);
+
+            setSelectedFile(null);
+            setImagePreview(null);
+            if (fileInputRef.current) {
+                fileInputRef.current.value = '';
+            }
         } catch (error) {
             console.error('Error uploading image:', error);
         }
     };
 
     return (
-        <div>
-            <input type="file" accept='*/image' onChange={handleFileChange} />
+        <div class="upload-container">
+            <label class="custom-file-input">
+                <input type="file" ref={fileInputRef} accept="image/*" onChange={handleFileChange} />
+                Choose Image
+            </label>
+            <span class="file-name">{selectedFile ? selectedFile.name : "No file selected"}</span>
+            {imagePreview && (
+                <div className="image-preview">
+                    <img src={imagePreview} alt="Selected Preview" />
+                </div>
+            )}
             <button onClick={handleUpload}>Add Image</button>
         </div>
+
     );
 };
 
