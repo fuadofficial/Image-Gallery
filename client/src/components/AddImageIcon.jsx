499bf4ed61971abf5d104ac1a0a1cef1cf730561
unknown <ahmadfuadoorakam@gmail.com>
2024-08-27 06:15:46 -0700

    added some advance styles

--- client/src/components/AddImageIcon.jsx
+++ client/src/components/AddImageIcon.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 const AddImageIcon = ({ onImageUpload }) => {
     const [selectedFile, setSelectedFile] = useState(null);
     const [imagePreview, setImagePreview] = useState(null);
-    const [uploading, setUploading] = useState(false); // For upload animation
+    const [uploading, setUploading] = useState(false);
     const fileInputRef = useRef(null);
 
     const handleFileChange = (event) => {
@@ -23,7 +23,7 @@ const AddImageIcon = ({ onImageUpload }) => {
         const formData = new FormData();
         formData.append('image', selectedFile);
 
-        setUploading(true); // Start uploading animation
+        setUploading(true);
 
         try {
             const response = await axios.post('http://localhost:5000/api/image/upload', formData);
@@ -37,33 +37,35 @@ const AddImageIcon = ({ onImageUpload }) => {
         } catch (error) {
             console.error('Error uploading image:', error);
         } finally {
-            setUploading(false); // End uploading animation
+            setUploading(false);
         }
     };
 
     return (
-        <div className="upload-container">
-            <label className="custom-file-input">
-                <input type="file" accept="image/*" onChange={handleFileChange} />
-                Choose Image
-            </label>
-            <span className="file-name">{selectedFile ? selectedFile.name : "No file selected"}</span>
-            {imagePreview && (
-                <div className="image-preview">
-                    <img src={imagePreview} alt="Selected Preview" />
+        <>
+            <div className="upload-container">
+                <div className='upload-container-input'>
+                    <label className="custom-file-input">
+                        <input type="file" accept="image/*" onChange={handleFileChange} />
+                        Choose Image
+                    </label>
+                    <span className="file-name">{selectedFile ? selectedFile.name : "No file selected"}</span>
+                    {imagePreview && (
+                        <div className="image-preview">
+                            <img src={imagePreview} alt="Selected Preview" />
+                        </div>
+                    )}
                 </div>
-            )}
-            <div className="button-container">
-                <button onClick={handleUpload} disabled={uploading}>
-                    {uploading ? 'Uploading...' : 'Add Image'}
-                </button>
-                {uploading && (
-                    <div className="progress-bar-container progress-bar-show">
-                        <div className="progress-bar"></div>
-                    </div>
-                )}
+                <div className="button-container">
+                    <button onClick={handleUpload} disabled={uploading}>
+                        {uploading ? 'Uploading...' : 'Add Image'}
+                    </button>
+                </div>
             </div>
-        </div>
+            <div className="progress-bar-container progress-bar-show">
+                <div className="progress-bar"></div>
+            </div>
+        </>
     );
 };
 
